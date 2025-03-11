a093af3f7819bf03828b869f712b4e64b2f2bf0d
VitoriaFrohlich <vitoriafrohlichh@gmail.com>
2025-03-10 21:26:07 -0300

    feat: criado modelo de carro esportivo

--- src/App.java
+++ src/App.java
@@ -2,10 +2,12 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
+        Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA,6, 45 );
+
 
         System.out.println("Tipos de veiculos:");
         System.out.println(basico);
-
+        System.out.println(esportivo);
         System.out.println("\n\n----------------");
         System.out.println("\nAbastencendo carro basico com gasolina");
         basico.abastece(TipoCombustivel.GASOLINA, 55);
